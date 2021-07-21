f856096a818cfa5631021d22e68fdd303d6744d7
Temerold <67090612+Temerold@users.noreply.github.com>
2021-07-21 14:47:27 +0200

    Format code
    Using: https://goonlinetools.com/lua-beautifier/

--- mine.lua
+++ mine.lua
@@ -3,8 +3,8 @@ local len = 8
 turtle.refuel(10)
 
 while true do
-    for d=1, len do
-        for i=1, len-1 do
+    for d = 1, len do
+        for i = 1, len - 1 do
             turtle.dig()
             turtle.forward()
         end
@@ -17,14 +17,13 @@ while true do
 
             turtle.dig()
             turtle.forward()
-            
+
             if (d % 2 == len % 2) then
                 turtle.turnLeft()
             else
                 turtle.turnRight()
             end
         end
-        
     end
     turtle.digDown()
     turtle.down()
